bec2ef62c5d78daaa1e2af9a89b72b15ef07279d
Taishin <2200169@ecccomp.com>
2023-11-28 10:27:18 +0900

    tableのコンポーネント化

--- src/router.js
+++ src/router.js
@@ -11,8 +11,9 @@ import Admin_signup from './components/Admin/Admin_signup.vue'
 import User_basic_page from './components/User/User_basic_page.vue'
 import Toilet_table from './components/Toilet_table.vue'
 
+
 const routes = [
-    { path: '/', component: Toilet_table ,}, //TopPageを表示
+    { path: '/', component: User_basic_page ,}, //TopPageを表示
     { path: '/:pathMatch(.*)*', redirect: '/' }, //存在しないパスにアクセスした場合はUser_basic_pageを表示
   ]
 
