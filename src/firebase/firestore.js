eec60d1de21a292240e9383a35f05090100ffdc3
Taishin <2200169@ecccomp.com>
2024-01-15 09:59:03 +0900

    保存

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -7,18 +7,18 @@ export const FireStore = reactive({
     Toilet_table_items_db: [],
 
     //firebaseから取得するID
-    Toilet_ID: "2wFxgIVbwJWUr3F17mbQ", //仮用のID
+    Toilet_ID: "mJWV67TP4brb6Iw0t3OP", //仮用のID
 
     //firebaseからデータ(Toiletコレクション)を取得する関数
     async getData() {
         const db = getFirestore()
         //firebaseからデータを取得
-        const docRef = doc(db, "test", Toilet_ID);
+        const docRef = doc(db, "Toilet", this.Toilet_ID);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
             console.log("Document data:", docSnap.data());
             //firebaseから取得したデータを格納
-            Toilet_table_items_db = docSnap.data();
+            this.Toilet_table_items_db = docSnap.data();
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
